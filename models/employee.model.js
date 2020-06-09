819ab137cbb7eb187043a46afd06c085ab30d7b4
Amirul Syafiq <syafiqsafie19@gmail.com>
2020-06-10 07:29:14 +0800

    insert to mongodb
    insert to mongodb

--- models/employee.model.js
+++ models/employee.model.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 var employeeSchema = new mongoose.Schema({
-    fullname : {
-        type : String
+    fullName : {
+        type : String,
+        required: 'This field is required.'
     },
     email : {
         type : String
@@ -14,5 +15,11 @@ var employeeSchema = new mongoose.Schema({
         type : String
     }
 });
+
+//validation email
+employeeSchema.path('email').validate((val) => {
+    emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(val);
+}, 'Invalid e-mail format.');
 
 mongoose.model('Employee', employeeSchema);
--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const express = require('express');
 const path = require('path');
 const exphbd = require('express-handlebars');
 const employeeController = require('./controllers/employeeController');
+const bodyparser = require('body-parser');
 
 var app = express();
-
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+app.use(bodyparser.json());
 app.set('views', path.join(__dirname, '/views/'));
 app.engine('hbs', exphbd({extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/'}));
 app.set('view engine', 'hbs');
